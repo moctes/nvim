01390872dc0a9314c1ccd369cee36edd3b96dcf3
Mocte Sandoval Vargas <mocte.lince@gmail.com>
2024-01-28 18:08:29 -0600

    Neotree, autoclose on select file

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+  },
+  {
+    -- Add indentation guides even on blank lines
+    "lukas-reineke/indent-blankline.nvim",
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = "ibl",
+    opts = {},
+  },
+}
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,26 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		require("neo-tree").setup({
+			event_handlers = {
+
+				{
+					event = "file_opened",
+					handler = function(file_path)
+						-- auto close
+						-- vimc.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
+	end,
 }
