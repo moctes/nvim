0058da1c71b8f977d5ee45fd8dc410a4c4b2a117
Mocte Sandoval Vargas <mocte.lince@gmail.com>
2024-01-29 20:49:40 -0600

    Added: nvim-ufo for folding text

---
+++ lua/plugins/nvimufo.lua
@@ -0,0 +1,14 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = "kevinhwang91/promise-async",
+
+	config = function()
+		local ufo = require("ufo")
+
+		ufo.setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "lsp", "indent" }
+			end,
+		})
+	end,
+}
