725f6f9a1c24961cc4c67d52a8d5e24d0c8e02d2
Mocte Sandoval Vargas <mocte.lince@gmail.com>
2024-02-05 11:24:17 -0600

    Noice tunning

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,23 +1,37 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-		routes = {
-			{
-				filter = { event = "notify", find = "No information available" },
-				opts = { skip = true },
-			},
-		},
-		presets = {
-			lsp_doc_border = true,
-		},
-	},
-	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
-	},
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    routes = {
+      {
+        filter = { event = "notify", find = "No information available" },
+        opts = { skip = true },
+      },
+    },
+    presets = {
+      lsp_doc_border = true,
+    },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
+  config = function()
+    require("noice").setup({})
+    vim.keymap.set("n", "<leader>nl", function()
+      require("noice").cmd("last")
+    end)
+
+    vim.keymap.set("n", "<leader>nh", function()
+      require("noice").cmd("history")
+    end)
+
+    vim.keymap.set("n", "<leader>nd", function()
+      require("noice").cmd("dismiss")
+    end)
+  end,
 }
