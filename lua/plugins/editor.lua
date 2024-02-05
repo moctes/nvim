8edc271a62d034075bc5890637c8b2a605cfa62a
Mocte Sandoval Vargas <mocte.lince@gmail.com>
2024-02-05 11:21:03 -0600

    New: Editor stuff, indentation lines anda statuscol

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,14 +1,36 @@
 return {
-  {
-    "numToStr/Comment.nvim",
-    opts = {},
-  },
-  {
-    -- Add indentation guides even on blank lines
-    "lukas-reineke/indent-blankline.nvim",
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = "ibl",
-    opts = {},
-  },
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
+	{
+		-- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {},
+	},
+	{
+		"luukvbaal/statuscol.nvim",
+		config = function()
+			local builtin = require("statuscol.builtin")
+			require("statuscol").setup({
+				-- configuration goes here, for example:
+				relculright = true,
+				segments = {
+				  { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+				  {
+				    sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+				    click = "v:lua.ScSa"
+				  },
+				  { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+				  {
+				    sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+				    click = "v:lua.ScSa"
+				  },
+				}
+			})
+		end,
+	},
 }
