ad3f9d4b0a03b227c3e74ff44f3602da486b871b
Mocte Sandoval Vargas <mocte.lince@gmail.com>
2024-02-06 05:46:55 -0600

    Lualine show filename and parent dir

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,20 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'dracula'
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						show_filename_only = false,
+						path = 4,
+					},
+				},
+			},
+		})
+	end,
 }
